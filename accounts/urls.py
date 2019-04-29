53a50ffdb6e2fcae07b668c1312dcb6e8c8acc26
Matissou <matisse.landais@gmail.com>
2019-04-29 18:03:45 +0200

    Page de login

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
+from django.contrib.auth.views import LoginView
 from . import views
 
 urlpatterns = [
     path('', views.home),
+    path('login/', LoginView.as_view(template_name='accounts/login.html'), name="login")
 ]
