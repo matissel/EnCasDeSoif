bbdd5c3e25c9d3d84b5edf21a087dfe4abe178aa
loicrosso <37541460+Loic-R@users.noreply.github.com>
2019-04-30 16:52:59 +0200

    add jquery and cool navbar

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,7 +3,6 @@ from django.contrib.auth.views import LoginView
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('index/', LoginView.as_view(template_name='index.html'), name="index"),
-    path('login/', LoginView.as_view(template_name='login.html'), name="login")
+    path('', views.index, name="index"),
+    path('login/', LoginView.as_view(template_name='accounts/login.html'), name="login")
 ]
