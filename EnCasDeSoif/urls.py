3e3a15108ce269f7ce726134994cda406f311d99
loicrosso <loic.rosso@grenoble-inp.org>
2019-04-30 12:28:55 +0200

    add redirect to home page

--- EnCasDeSoif/urls.py
+++ EnCasDeSoif/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('accounts.urls')),
+    path('', RedirectView.as_view(url='/account/', permanent=True)),
 ]
