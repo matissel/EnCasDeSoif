3b347c1088292e0b193cb77456db91e29f17aee5
Matissou <matisse.landais@gmail.com>
2019-04-30 17:22:15 +0200

    Fix PR

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
-    path('login/', LoginView.as_view(template_name='accounts/login.html'), name="login")
+    path('login/', LoginView.as_view(template_name='accounts/login.html'), name="login"),
     path('logout/', LogoutView.as_view(template_name='accounts/logout.html'), name="logout"),
     path('register/', views.register, name="register"),
     path('profile/', views.view_profile, name='view_profile'),
