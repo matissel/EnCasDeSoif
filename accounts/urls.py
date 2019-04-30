754b87bf0fd4e0c9b147a33c5e1625669cdd7462
loicrosso <loic.rosso@grenoble-inp.org>
2019-04-30 13:18:46 +0200

    add index page

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,6 +3,7 @@ from django.contrib.auth.views import LoginView
 from . import views
 
 urlpatterns = [
-    path('', views.home),
-    path('login/', LoginView.as_view(template_name='accounts/login.html'), name="login")
+    path('', views.index),
+    path('index/', LoginView.as_view(template_name='index.html'), name="index"),
+    path('login/', LoginView.as_view(template_name='login.html'), name="login")
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -7,4 +7,7 @@ def home(request):
     numbers = [1,2,3,4,5]
     name = "Matisse"
     args = {'name':name,'numbers':numbers}
-    return render(request, 'accounts/home.html', args)
+    return render(request, 'home.html', args)
+
+def index(request):
+    return render(request, 'index.html')
