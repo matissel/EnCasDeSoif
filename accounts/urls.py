f7ecc4253fe1b91c4f9672f73b05cfcdd2ebe05a
Matissou <matisse.landais@gmail.com>
2019-04-30 15:36:42 +0200

    Page de logout

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 from . import views
 
 urlpatterns = [
     path('', views.home),
-    path('login/', LoginView.as_view(template_name='accounts/login.html'), name="login")
+    path('login/', LoginView.as_view(template_name='accounts/login.html'), name="login"),
+    path('logout/', LogoutView.as_view(template_name='accounts/logout.html'), name="logout"),
 ]
