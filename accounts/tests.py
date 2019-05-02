ab1a3ec529cd1fde5a4b9dc683f8e91cf2b31edd
loicrosso <37541460+Loic-R@users.noreply.github.com>
2019-05-02 17:05:58 +0200

    add very simple test

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
 # Create your tests here.
+class SimpleTestCase(TestCase):
+    def setUp(self):
+        pass
+
+    def test_allo(self):
+            add = 1 + 1
+            self.assertEqual(2, add)
