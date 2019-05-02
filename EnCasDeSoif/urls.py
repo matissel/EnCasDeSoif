7785b841e88e5caa96806a0b2a21c7ccd5681042
Matissou <matisse.landais@gmail.com>
2019-05-02 15:01:53 +0200

    Redirection

--- EnCasDeSoif/urls.py
+++ EnCasDeSoif/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
-    path('', views.login_redirect, name='login_redirect'),
+    path('', TemplateView.as_view(template_name='index.html') , name='index'),
     path('admin/', admin.site.urls),
     path('account/', include('accounts.urls')),
     #path('', RedirectView.as_view(url='/account/', permanent=True)),
