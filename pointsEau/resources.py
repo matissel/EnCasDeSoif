538f62daa242eb44341ff76a585987b204e9a540
Matissou <matisse.landais@gmail.com>
2019-05-04 17:47:47 +0200

    Correction d'erreur 401

--- pointsEau/resources.py
+++ pointsEau/resources.py
@@ -1,8 +1,9 @@
 from tastypie.resources import ModelResource
 from .models import PointEau
+from tastypie.authorization import Authorization
 
 class PointEauResource(ModelResource):
     class Meta: 
         queryset = PointEau.objects.all()
         resource_name='points'
-    
+        authorization = Authorization()    
