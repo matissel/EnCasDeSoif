395bb928badc00b80d3648bf8edeb584571f6d9e
Matissou <matisse.landais@gmail.com>
2019-05-07 14:14:23 +0200

    Serializer et render view

--- pointsEau/views.py
+++ pointsEau/views.py
@@ -5,6 +5,7 @@ from .serializers import PointEauSerializer
 from .models import PointEau
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.renderers import JSONRenderer
 
 
 # Create your views here.
@@ -25,10 +26,15 @@ def addPE(request):
 class ListPointEauView(APIView):
     #queryset = PointEau.objects.all()
     #serializer_class = PointEauSerializer
+    renderer_classes = (JSONRenderer, )
     def get(self,request):
         allpe = PointEau.objects.all()
         serializer = PointEauSerializer(allpe, many=True)
-        return Response({"allpe":serializer.data})
+        #return Response({"allpe":serializer.data})
+        args = {
+            'allpe':serializer.data
+        }
+        return render(request, 'index.html',  args)
     
 
 def afficherPointsEau(request):
@@ -39,4 +45,4 @@ def afficherPointsEau(request):
         'points' : serializer.data,
     }
 
-    return render(request, 'pointsEau/testmap.html',  args)
+    return render(request, 'index.html',  args)
