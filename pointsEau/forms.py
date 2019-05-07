9c25007a5c9057311030c98b4a98bca563afe82f
Matissou <matisse.landais@gmail.com>
2019-05-07 14:04:05 +0200

    form PE

--- pointsEau/forms.py
+++ pointsEau/forms.py
@@ -4,14 +4,14 @@ from .models import PointEau
 class PointEauForm(ModelForm):
     class Meta:
         model = PointEau
-        fields = ['nom', 'lat','long']
+        fields = ['nom', 'lat','long', 'desc']
 
     def save(self, commit=True):
         pointEau = super(ModelForm, self).save(commit=False)
         pointEau.nom = self.cleaned_data['nom']
         pointEau.lat = self.cleaned_data['lat']
         pointEau.long = self.cleaned_data['long']
-
+        pointEau.desc = self.cleaned_data['desc']
         if commit:
             pointEau.save()
         
