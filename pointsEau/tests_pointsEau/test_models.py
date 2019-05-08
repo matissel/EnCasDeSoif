cce4b43fbc96b8fafa3c20b39ebaa9211576ddc8
loicrosso <37541460+Loic-R@users.noreply.github.com>
2019-05-08 15:19:06 +0200

    change lat long values

--- pointsEau/tests_pointsEau/test_models.py
+++ pointsEau/tests_pointsEau/test_models.py
@@ -10,9 +10,9 @@ class SimpleTestPointEau(TestCase):
     # Create a user and add 3 points eau for him
     def setUp(self):
         user = User.objects.create_user(username='temporary', email='temporary@gmail.com', password='temporary')
-        PointEau.objects.create(nom="Premier point", lat=99, long=66, desc="A test point eau", owner=user)
-        PointEau.objects.create(nom="point deux", lat=123, long=54, desc="A test point eau", owner=user)
-        PointEau.objects.create(nom="point trois", lat=9879, long=2254, desc="A test point eau", owner=user)
+        PointEau.objects.create(nom="Premier point", lat=43.099999999, long=34.0000000009, desc="A test point eau", owner=user)
+        PointEau.objects.create(nom="point deux", lat=12.099999999, long=54.099999999, desc="A test point eau", owner=user)
+        PointEau.objects.create(nom="point trois", lat=34.0099999999, long=22.0099999999, desc="A test point eau", owner=user)
 
     def testGetPointEauBasique(self):
         peau = PointEau.objects.all()
