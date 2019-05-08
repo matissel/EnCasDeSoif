e32a6b83aac0ecd7c222196543002b3e06da2268
Matissou <matisse.landais@gmail.com>
2019-05-08 12:12:05 +0200

    Changement de l'adresse pour l'api, plus d'erreur sur le make'

--- EnCasDeSoif/views.py
+++ EnCasDeSoif/views.py
@@ -11,31 +11,31 @@ def login_redirect(request):
 def index(request):
     existingPoints = PointEau.objects.all()
     mapbox_access_token = "pk.eyJ1IjoibWF0aXNzb3UiLCJhIjoiY2plOGFtdWhvMDZuNzMzcHIxZTNuMXo0dSJ9.aPI9ecTNZg0-ExUGEPX14w"
-    url = "http://127.0.0.1:8000/api/all"
+    url = "http://localhost:8000/api/all"
     toutLesPoints = requests.get(url=url).json()
 
     geojson = {
         'type': 'FeatureCollection',
-        'features' : []
+        'features': []
     }
 
     for point in toutLesPoints['allpe']:
 
         nouveauPoint = {
             'type': 'Feature',
-            'geometry' : {
-                'type' : 'Point',
+            'geometry': {
+                'type': 'Point',
                 'coordinates': [point['long'], point['lat']]
             },
             'properties': {
-                'title' : point['nom'],
-                'description' : point['desc']
+                'title': point['nom'],
+                'description': point['desc']
             }
         }
         geojson['features'].append(nouveauPoint)
 
     args = {
         'mapbox_access_token': mapbox_access_token,
-        'allpe' : geojson
+        'allpe': geojson
     }
     return render(request, 'index.html', args)
