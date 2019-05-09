a9888a595c657333ca6ca600e7d592409cfac1c7
loicrosso <37541460+Loic-R@users.noreply.github.com>
2019-05-09 15:58:26 +0200

    remove unused setting

--- EnCasDeSoif/settings.py
+++ EnCasDeSoif/settings.py
@@ -97,11 +97,6 @@ if 'TRAVIS' in os.environ:
         }
     }
 
-# used to populate database
-FIXTURE_DIRS = (
-    '/pointsEau/fixtures/',
-)
-
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
