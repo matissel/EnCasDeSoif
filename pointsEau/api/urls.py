a63d22841f37dc25a8a3eadc00280a310d61e192
Matissou <matisse.landais@gmail.com>
2019-05-12 20:10:12 +0200

    Suppression browsable API

--- pointsEau/api/urls.py
+++ pointsEau/api/urls.py
@@ -1,8 +1,7 @@
 from rest_framework import routers
-from .views import PointEauViewSet, AdminPointEauView
+from .views import PointEauViewSet
 from django.urls import path, include
 
 router = routers.DefaultRouter()
 router.register(r'pointseau', PointEauViewSet, base_name='pointseau')
-router.register(r'pointseauadmin', AdminPointEauView, base_name="pointseauadmin")
 urlpatterns = router.urls
--- pointsEau/api/views.py
+++ pointsEau/api/views.py
@@ -1,20 +1,13 @@
 from .serializers import PointEauSerializer
 from rest_framework import viewsets
-from rest_framework.renderers import JSONRenderer, BrowsableAPIRenderer
+from rest_framework.renderers import JSONRenderer
 from pointsEau.models import PointEau
 from pointsEau.forms import PointEauForm
 from rest_framework.response import Response
 from .permissions import IsGetOrIsAuthenticated
-from rest_framework.permissions import IsAdminUser
 
 class PointEauViewSet(viewsets.ModelViewSet):
     renderer_classes = (JSONRenderer,)
     queryset = PointEau.objects.all()
     serializer_class = PointEauSerializer
     permission_classes = [IsGetOrIsAuthenticated, ]
-
-class AdminPointEauView(viewsets.ModelViewSet):
-    renderer_classes = (BrowsableAPIRenderer,)
-    queryset = PointEau.objects.all()
-    serializer_class = PointEauSerializer
-    permission_classes = [IsAdminUser ]
