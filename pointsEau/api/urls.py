24d09591d0e23cd80e8ce3476bdccdacab6153af
loicrosso <37541460+Loic-R@users.noreply.github.com>
2019-05-15 19:36:45 +0200

    allow to list and modify pointeau

--- pointsEau/api/urls.py
+++ pointsEau/api/urls.py
@@ -1,7 +1,12 @@
 from rest_framework import routers
-from .views import PointEauViewSet
+from . import views 
 from django.urls import path, include
 
-router = routers.DefaultRouter()
-router.register(r'pointseau', PointEauViewSet, base_name='pointseau')
-urlpatterns = router.urls
+# router = routers.DefaultRouter()
+# router.register(r'pointseau', PointEauViewSet, base_name='pointseau')
+# urlpatterns = router.urls
+
+urlpatterns = [
+    path('pointseau/', views.points_eau_list),
+    path('pointseau/<int:pk>/', views.points_eau_detail),
+]
--- pointsEau/api/views.py
+++ pointsEau/api/views.py
@@ -1,8 +1,9 @@
 from .serializers import PointEauSerializer
 from rest_framework import viewsets
 from rest_framework.renderers import JSONRenderer
+from rest_framework.decorators import api_view
 from pointsEau.models import PointEau
-from pointsEau.forms import PointEauForm
+from rest_framework import status
 from rest_framework.response import Response
 from .permissions import IsGetOrIsAuthenticated
 
@@ -11,3 +12,39 @@ class PointEauViewSet(viewsets.ModelViewSet):
     queryset = PointEau.objects.all()
     serializer_class = PointEauSerializer
     permission_classes = [IsGetOrIsAuthenticated, ]
+
+@api_view(['GET', 'POST'])
+def points_eau_list(request):
+    if request.method == 'GET':
+        list_pe = PointEau.objects.all()
+        serializer = PointEauSerializer(list_pe, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = PointEauSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def points_eau_detail(request, pk):
+    try:
+        pe = PointEau.objects.get(pk=pk)
+    except PointEau.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = PointEauSerializer(pe)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = PointEauSerializer(pe, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        pe.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
