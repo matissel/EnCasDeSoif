19716663b83404bbb5c0068961b27cc2113d34d1
loicrosso <37541460+Loic-R@users.noreply.github.com>
2019-05-17 15:51:53 +0200

    correction du changement de mdp

--- accounts/urls.py
+++ accounts/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
     path('register/', views.register, name="register"),
     path('profile/', views.view_profile, name='view_profile'),
     path('profile/edit/', views.edit_profile, name='edit_profile'),
-    path('password/', views.change_password, name='change_password'),
+    path('profile/password/', views.change_password, name='change_password'),
 
 ]
