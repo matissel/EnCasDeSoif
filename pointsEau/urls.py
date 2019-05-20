4442dcc451581d0911b017b1e9f3c840f2a566be
loicrosso <37541460+Loic-R@users.noreply.github.com>
2019-05-20 17:10:21 +0200

    ajout suppression de point d'eau

--- pointsEau/urls.py
+++ pointsEau/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
-from .views import addPE, init, editPE, viewPE
+from .views import addPE, init, editPE, viewPE, delPE
 from .pointEau_api import afficherToutPoints
 
 urlpatterns = [
     path('nouveau/', addPE, name="newPE"),
     path('lister', viewPE, name="listPE"),
     path('editer/<int:pk>', editPE, name="editPE"),
+    path('supprimer/<int:pk>', delPE, name="delPE"),
     path('init', init, name="init")
 ]
