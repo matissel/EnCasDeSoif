02f7dddf1519f36f0362472d43a18153f6760c82
Matissou <matisse.landais@gmail.com>
2019-05-23 16:11:23 +0200

    Test update et update nn autorisé

--- pointsEau/tests_pointsEau/test_api.py
+++ pointsEau/tests_pointsEau/test_api.py
@@ -37,3 +37,46 @@ class PointEauAPITest(APITestCase, TestCase):
         self.assertEqual(float(parsed['long']), 22.12345678 )
         self.assertEqual(parsed['desc'], "A test point eau")
         self.assertEqual(parsed['owner'], self.user.username)
+    
+    def test_update_pointeau(self):
+        url = reverse('pointeau-detail', kwargs={'pk': self.pe.id})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        
+        parsed = response.json()
+        previousLat = float(parsed['lat'])
+        data = {
+            "nom": parsed['nom'],
+            "lat": "45.4",
+            "long": parsed['long'],
+            "desc": parsed['desc'],
+            "owner" : parsed['owner']
+        }        
+        response = self.client.put(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        parsedAfter = response.json()
+        self.assertNotEqual(float(45.4), float(previousLat))
+        
+    def test_putNotOwner_pointeau(self):
+        url = reverse('pointeau-detail', kwargs={'pk': self.pe.id})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        data = {
+            "nom": "Point eau test api",
+            "lat": "43.1235678",
+            "long": "34.12345678",
+            "desc": "Point eau 1",
+            "owner" : self.user.id
+        }
+        userT = User.objects.create_user(username='meanUser',email='imMean@gmail.com' ,password='12345')
+        login = self.client.force_login(userT)
+        response = self.client.put(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    # Test creation owner different 
+    # Test not logged in for create
+    # Test changement d'owner
+    # Test suppression 
+    # Test impossible d'acceder a l'objet (owner)
+        
