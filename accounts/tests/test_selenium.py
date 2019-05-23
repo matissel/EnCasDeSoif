fbe6e5de4fd0afa1f746ab627627827890b32be0
loicrosso <37541460+Loic-R@users.noreply.github.com>
2019-05-23 17:01:51 +0200

    add test for login success

--- accounts/tests/test_selenium.py
+++ accounts/tests/test_selenium.py
@@ -1,17 +1,20 @@
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium.webdriver.chrome.webdriver import WebDriver
+from django.contrib.auth.models import User
 
 class MySeleniumTests(StaticLiveServerTestCase):
 
     @classmethod
-    def setUpClass(cls):
+    def setUpClass(self):
         super().setUpClass()
-        cls.selenium = WebDriver()
-        cls.selenium.implicitly_wait(10)
+        self.selenium = WebDriver()
+        # self.selenium.implicitly_wait(10)
+        self.myGoodTestPassword = 'aTestPassword'
+        self.user = User.objects.create_user(username='testuser', email='testUser@test.com', password=self.myGoodTestPassword)
 
     @classmethod
-    def tearDownClass(cls):
-        cls.selenium.quit()
+    def tearDownClass(self):
+        self.selenium.quit()
         super().tearDownClass()
 
     def test_login_failed(self):
@@ -28,3 +31,18 @@ class MySeleniumTests(StaticLiveServerTestCase):
         #Test that error messages are displayed
         error_message = self.selenium.find_elements_by_xpath('//ul[@class="errorlist nonfield"]')
         self.assertGreater(len(error_message), 0, "Wrong login should return at least 1 error message")
+
+    def test_login_success(self):
+        self.selenium.get('%s%s' % (self.live_server_url, '/account/login/'))
+        username_input = self.selenium.find_element_by_name("username")
+        username_input.send_keys(self.user.username)
+        password_input = self.selenium.find_element_by_name("password")
+        password_input.send_keys(self.myGoodTestPassword)
+        self.selenium.find_element_by_xpath('//button[@type="submit" and text()="Se connecter"]').click()
+        
+        #Test we stay on the same page
+        page_url = self.selenium.current_url
+        self.assertEqual(page_url, '%s%s' % (self.live_server_url, '/'), "Good login should redirect on index page")
+        #Test that no error messages are displayed
+        error_message = self.selenium.find_elements_by_class_name("errorlist")
+        self.assertEqual(len(error_message), 0, "Good login, no error message shloud be displayed")
