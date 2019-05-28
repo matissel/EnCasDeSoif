2f98d232e35a35b1e9c3d47729483aed87dc7a5c
loicrosso <37541460+Loic-R@users.noreply.github.com>
2019-05-28 12:05:48 +0200

    remove cache

--- EnCasDeSoif/settings/prod.py
+++ EnCasDeSoif/settings/prod.py
@@ -4,12 +4,6 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['*', ]
 
-CACHES = {
-    'default': {
-        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
-        'LOCATION': '127.0.0.1:11211',
-    }
-}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
