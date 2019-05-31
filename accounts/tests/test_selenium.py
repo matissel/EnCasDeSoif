f924488349046d1f1b85bf60e512749b6d3e00dc
loicrosso <37541460+Loic-R@users.noreply.github.com>
2019-05-31 18:30:26 +0200

    change tests

--- accounts/tests/test_selenium.py
+++ accounts/tests/test_selenium.py
@@ -2,9 +2,9 @@ from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from django.contrib.auth.models import User
 
+
 class MySeleniumTests(StaticLiveServerTestCase):
 
-    
     def setUp(self):
         options = webdriver.ChromeOptions()
         options.add_argument('headless')
@@ -12,10 +12,9 @@ class MySeleniumTests(StaticLiveServerTestCase):
         self.myGoodTestPassword = 'aTestPassword'
         self.user = User.objects.create_user(username='testuser', email='testUser@test.com', password=self.myGoodTestPassword)
 
-    
     def tearDown(self):
         self.driver.quit()
-    
+
     def login(self, username, password):
         self.driver.get('%s%s' % (self.live_server_url, '/account/login/'))
         username_input = self.driver.find_element_by_name("username")
@@ -26,36 +25,36 @@ class MySeleniumTests(StaticLiveServerTestCase):
 
     def test_login_wrong_user(self):
         self.login('wrongUser', self.myGoodTestPassword)
-        #Test we stay on the same page
+        # Test we stay on the same page
         page_url = self.driver.current_url
         self.assertEqual(page_url, '%s%s' % (self.live_server_url, '/account/login/'), "Wrong login should stay on the same login page")
-        #Test that error messages are displayed
-        error_message = self.driver.find_elements_by_xpath('//ul[@class="errorlist nonfield"]')
+        # Test that error messages are displayed
+        error_message = self.driver.find_elements_by_xpath('//div[@class="alert alert-danger"]')
         self.assertGreater(len(error_message), 0, "Wrong login should return at least 1 error message")
 
     def test_login_wrong_password(self):
         self.login(self.user.username, 'wrongPassword')
-        #Test we stay on the same page
+        # Test we stay on the same page
         page_url = self.driver.current_url
         self.assertEqual(page_url, '%s%s' % (self.live_server_url, '/account/login/'), "Wrong login should stay on the same login page")
-        #Test that error messages are displayed
-        error_message = self.driver.find_elements_by_xpath('//ul[@class="errorlist nonfield"]')
+        # Test that error messages are displayed
+        error_message = self.driver.find_elements_by_xpath('//div[@class="alert alert-danger"]')
         self.assertGreater(len(error_message), 0, "Wrong login should return at least 1 error message")
 
     def test_login_wrong_user_password(self):
         self.login('wrongUser', 'wrongPassword')
-        #Test we stay on the same page
+        # Test we stay on the same page
         page_url = self.driver.current_url
         self.assertEqual(page_url, '%s%s' % (self.live_server_url, '/account/login/'), "Wrong login should stay on the same login page")
-        #Test that error messages are displayed
-        error_message = self.driver.find_elements_by_xpath('//ul[@class="errorlist nonfield"]')
+        # Test that error messages are displayed
+        error_message = self.driver.find_elements_by_xpath('//div[@class="alert alert-danger"]')
         self.assertGreater(len(error_message), 0, "Wrong login should return at least 1 error message")
 
     def test_login_success(self):
         self.login(self.user.username, self.myGoodTestPassword)
-        #Test we stay on the same page
+        # Test we stay on the same page
         page_url = self.driver.current_url
         self.assertEqual(page_url, '%s%s' % (self.live_server_url, '/'), "Good login should redirect on index page")
-        #Test that no error messages are displayed
+        # Test that no error messages are displayed
         error_message = self.driver.find_elements_by_class_name("errorlist")
         self.assertEqual(len(error_message), 0, "Good login, no error message shloud be displayed")
